da7b5c5fd2706972cc2edf55a603fe248ebff0d9
Alex Lewis <ebcomalex@gmail.com>
2012-02-04 14:20:39 -0500

    haml, thin, pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Pausebreaklandingpage::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/about"
+
+  get "pages/contact"
+  
+  root :to => "pages#home"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
