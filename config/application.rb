f0bcb455d3830860cd11046a94799d0a4e5678ca
Brian Chia <briancchia@gmail.com>
2012-02-22 16:50:16 -0500

    Configured HAML output in Rails. Installed and conifigured Compass-Rails. Recreated homepage and styling.

--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,11 @@ module Pausebreaklandingpage
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
+    # Rails 3 HAML Generator
+    config.generators do |g|
+      g.template_engine :haml
+    end
+
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
