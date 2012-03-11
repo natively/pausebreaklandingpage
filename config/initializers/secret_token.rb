2234bfc512be0ecb230703bdd0908b20623edf82
Alex Lewis <ebcomalex@gmail.com>
2012-03-11 16:40:42 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pausebreaklandingpage::Application.config.secret_token = 'fbb978efec97bc03d25a55bc6e4ebdfcf0f77ac6d554a0ac58b97b3e79ee87240d58a1a3c8ea9d7e150e03aa9289a73596f28f3d3efc424ceac4f1fbcbab8e01'
