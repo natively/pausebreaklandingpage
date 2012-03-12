b3f37dce697df1c81a167b57d84a4ab213e7539b
Alex Lewis <ebcomalex@gmail.com>
2012-03-12 15:27:44 -0400

    lowercase, standardized branding

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # Returns the full title on a per-page basis.
     def full_title(page_title)
-      base_title = "Pause|Break"
+      base_title = "pause|break"
       if page_title.empty?
         base_title
       else
