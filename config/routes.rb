a12c9eaf6f0616a71490bbd8c66cbc331ef9e637
Alex Lewis <ebcomalex@gmail.com>
2012-03-14 00:22:12 -0400

    bio page => /staic_pages/about

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,14 @@ Pausebreaklandingpage::Application.routes.draw do
   match '/' => "static_pages#home", :as => :home
   match '/contact' => "static_pages#contact", :as => :contact
   match '/thank_you' => "static_pages#thank_you", :as => :thank_you
+  match '/about' => "static_pages#about", :as => :about
+
   match '/potential_clients' => "potential_clients#create", :as => :potential_clients
 
   get "static_pages/home"
   get "static_pages/contact"
   get "static_pages/thank_you"
+  get "static_pages/about"
 
 
   # The priority is based upon order of creation:
