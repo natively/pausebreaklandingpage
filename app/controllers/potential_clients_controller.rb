00cb6bbd308eda686c39d7f194f1c14d172399fa
Alex Lewis <ebcomalex@gmail.com>
2012-12-21 00:33:57 -0500

    more revisions to marketing page

--- app/controllers/potential_clients_controller.rb
+++ app/controllers/potential_clients_controller.rb
@@ -2,6 +2,7 @@ class PotentialClientsController < ApplicationController
   respond_to :json
 
   def create
-    respond_with PotentialClient.create( params[ :potential_client ] )
+    respond_with(PotentialClient.create( params[ :potential_client ] ).as_json,
+      :location => 'nil')
   end
 end
--- app/models/potential_client.rb
+++ app/models/potential_client.rb
@@ -1,8 +1,8 @@
 class PotentialClient < ActiveRecord::Base
   attr_accessible :name, :email, :phone, :project_description
 
-  validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true
-  validates :phone, presence: true
-  validates :project_description, presence: true
+  #validates :name, presence: true, length: { maximum: 50 }
+  #validates :email, presence: true
+  #validates :phone, presence: true
+  #validates :project_description, presence: true
 end
